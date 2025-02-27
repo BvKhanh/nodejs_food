82456ee7e842592ba26442255a30f993c542596f
Bui Viet Khanh <khanhbuiviet3.com>
2025-02-27 21:30:47 +0700

    update_3

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
 
-async function connect() {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/paradise_food_dev');
-        console.log('Ket noi thanh cong MongoDB');
-    } catch (error) {
-        console.log('Ket noi that bai MongoDB', error);
-    }
-}
-
-module.exports = { connect };
