fcc8dafda01ecf2383100d8cd914aca65741926a
Bui Viet Khanh <khanhbuiviet3.com>
2025-02-28 12:21:57 +0700

    update 28/2

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
-const db = require('./config/db');
+// const db = require('./config/db');
 
-db.connect();
+// db.connect();
 
 route(app);
 
